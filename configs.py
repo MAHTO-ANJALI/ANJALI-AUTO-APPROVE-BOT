7ccbab3ee773995e2ce33cb3e1795e04e8e59a97
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-04-01 23:18:58 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
+    API_ID = int(getenv("API_ID", "28436511"))
+    API_HASH = getenv("API_HASH", "0114c36706aca2c07acc12046c15e3f6")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7000604952:AAEY-G6ieuK53EG-vPDBEu3-tINF-2XgiXM")
+    FSUB = getenv("FSUB", "AnjaliWorldOfficialBot")
+    CHID = int(getenv("CHID", "-1001970031336"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://AnjaliWorld098:anjaliworld098@cluster0.2xwkwyi.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
